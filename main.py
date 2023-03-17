1dd9a2ae7606e1aee0c43dc45243ae78f3841c76
Caíque <dev_caiquews@outlook.com>
2023-03-17 20:16:20 -0300

    user e password

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-
+'''
 maca = float(0.50)
 cadastro = input("Você está cadastrado? s ou n: ")
 qntdd = int(input("Quantas maçãs deseja comprar? : "))
@@ -9,3 +9,26 @@ if cadastro == "s" or qntdd>=20:
     print(f"Valor total: {maca*qntdd}")
 else:
     print(f"Valor total: {maca * qntdd}")
+
+'''
+
+senha = 12345
+usuario = "admin"
+
+user = input("Usuário: ")
+pswrd = int(input("Senha: "))
+
+if user == usuario and pswrd == senha:
+    print(f"Bem vindo {usuario}!!! :)")
+elif user == usuario and pswrd != senha:
+    rede = input("Deseja redefinir sua senha? (s/n): ")
+    if rede == "s":
+        senha = int(input("Qual sua nova senha? : "))
+        print(f"Sua nova senha : {senha}")
+    else:
+        print("Então escreve a senha certa FDP!!!")
+elif user != usuario and pswrd == senha:
+    print("Usuário ou Senha incorretos!!!")
+
+else:
+    print("KKKKKKKKKK Burro pra krl KKKKKKKKKK")
